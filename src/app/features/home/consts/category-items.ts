9c7a4bc87ee00b31c31661216f21cc8c1baad96d
Oksana Shvanchenkova <oksanashvanchenkova@gmail.com>
2026-02-16 19:41:47 +0100

    add heder

---
+++ src/app/features/home/consts/category-items.ts
@@ -0,0 +1,58 @@
+import { ICategory } from "../interfaces/category.interface";
+
+export const CATEGORY_ITEMS: ICategory[] = [
+    {
+        name: 'Książki',
+        iconSrc: '../../../assets/icons/books-icon.png',
+        linkData: '/tabs/catalog',
+        routerData: 'books'
+    },
+    {
+        name: 'Ubrania',
+        iconSrc: '../../../assets/icons/clothes.png',
+        linkData: '/tabs/catalog',
+        routerData: 'clothes'
+    },
+    {
+        name: 'Obuwie',
+        iconSrc: '../../../assets/icons/shoes.png',
+        linkData: '/tabs/catalog',
+        routerData: 'shoes'
+    },
+    {
+        name: 'Książki',
+        iconSrc: '../../../assets/icons/books-icon.png',
+        linkData: '/tabs/catalog',
+        routerData: 'books'
+    },
+    {
+        name: 'Ubrania',
+        iconSrc: '../../../assets/icons/clothes.png',
+        linkData: '/tabs/catalog',
+        routerData: 'clothes'
+    },
+    {
+        name: 'Obuwie',
+        iconSrc: '../../../assets/icons/shoes.png',
+        linkData: '/tabs/catalog',
+        routerData: 'shoes'
+    },
+    {
+        name: 'Książki',
+        iconSrc: '../../../assets/icons/books-icon.png',
+        linkData: '/tabs/catalog',
+        routerData: 'books'
+    },
+    {
+        name: 'Ubrania',
+        iconSrc: '../../../assets/icons/clothes.png',
+        linkData: '/tabs/catalog',
+        routerData: 'clothes'
+    },
+    {
+        name: 'Obuwie',
+        iconSrc: '../../../assets/icons/shoes.png',
+        linkData: '/tabs/catalog',
+        routerData: 'shoes'
+    }
+]
