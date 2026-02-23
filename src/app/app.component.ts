f6104548a8c06a0f195c679c1108853006607510
Oksana Shvanchenkova <oksanashvanchenkova@gmail.com>
2026-02-23 21:23:25 +0100

    add style correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { HeaderComponent } from './layout/header/header.component';
-import { IonButton, IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet, HeaderComponent, IonContent],
+  imports: [IonApp, IonRouterOutlet, HeaderComponent],
 })
 export class AppComponent {
   constructor() { }
--- src/app/core/consts/category-items.ts
+++ src/app/core/consts/category-items.ts
@@ -5,56 +5,56 @@ export const CATEGORY_ITEMS: ICategory[] = [
     {
         name: 'Ubrania',
         iconSrc: '../../../assets/icons/clothes.png',
-        linkData: '/tabs/catalog',
+        linkData: '/catalog',
         routerData: 'clothes'
     },
 
     {
         name: 'Książki',
         iconSrc: '../../../assets/icons/books-icon.png',
-        linkData: '/tabs/catalog',
+        linkData: '/catalog',
         routerData: 'books'
     },
     {
         name: 'Obuwie',
         iconSrc: '../../../assets/icons/shoes.png',
-        linkData: '/tabs/catalog',
+        linkData: '/catalog',
         routerData: 'shoes'
     },
     {
         name: 'Elektronika',
         iconSrc: '../../../assets/icons/electronics.png',
-        linkData: '/tabs/catalog',
+        linkData: '/catalog',
         routerData: 'electronics'
     },
     {
         name: 'Maluchy',
         iconSrc: '../../../assets/icons/babies.png',
-        linkData: '/tabs/catalog',
+        linkData: '/catalog',
         routerData: 'babies'
     },
     {
         name: 'Sport',
         iconSrc: '../../../assets/icons/sport.png',
-        linkData: '/tabs/catalog',
+        linkData: '/catalog',
         routerData: 'sport'
     },
     {
         name: 'Toys',
         iconSrc: '../../../assets/icons/toys.png',
-        linkData: '/tabs/catalog',
+        linkData: '/catalog',
         routerData: 'toys'
     },
     {
         name: 'School',
         iconSrc: '../../../assets/icons/school.png',
-        linkData: '/tabs/catalog',
+        linkData: '/catalog',
         routerData: 'school'
     },
     {
         name: 'Transport',
         iconSrc: '../../../assets/icons/transport.png',
-        linkData: '/tabs/catalog',
+        linkData: '/catalog',
         routerData: 'transport'
     }
 
