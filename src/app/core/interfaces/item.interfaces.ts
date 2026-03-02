4f08994e7fc1c18109f9717039bfca5b1bc9f438
Oksana Shvanchenkova <oksanashvanchenkova@gmail.com>
2026-03-02 18:24:59 +0100

    add item

--- src/app/core/interfaces/item.interfaces.ts
+++ src/app/core/interfaces/item.interfaces.ts
@@ -1,16 +1,20 @@
 export interface Item {
     id: number;
     user_id: number;
-    category: string;
+    category_id: string;
     title: string;
     description: string;
     item_condition: string;
     image: string;
     price: string;
+    status: string;
     created_at: string;
+    deleted_at: string | null;
+    updated_at: string
 }
 
 export interface IItemsDto {
+    success: boolean;
     data: Item[];
     meta: {
         currentPage: number;
