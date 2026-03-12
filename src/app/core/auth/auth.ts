ad667a14df902b50450dd28ef25dedcc159e9a25
Oksana Shvanchenkova <oksanashvanchenkova@gmail.com>
2026-03-12 20:14:13 +0100

    add registration user

--- src/app/core/auth/auth.ts
+++ src/app/core/auth/auth.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IUserRegistration } from '../interfaces/auth.interfaces';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
-  
+  private http = inject(HttpClient);
+
+  userRegistration(userData: IUserRegistration): Observable<any> {
+    return this.http.post<IUserRegistration>(`http://192.168.0.205:3000/auth/signup`, userData);
+  }
 }
